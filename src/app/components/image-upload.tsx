d2bc1b86f64527b2a54b7daf7544c196344ba2c4
cqlyj <luoyingjie0721@gmail.com>
2024-04-20 21:49:05 +0800

    update home page and the upload component

--- src/app/components/image-upload.tsx
+++ src/app/components/image-upload.tsx
@@ -3,27 +3,30 @@
 import { UploadDropzone } from "@/utils/uploadthing";
 import Image from "next/image";
 import { useState } from "react";
+import Result from "./result";
 
 export const ImageUpload = () => {
   const [imageURL, setImageURL] = useState<string>("");
 
   return (
-    <div>
-      <UploadDropzone
-        endpoint="strictImageAttachment"
-        onClientUploadComplete={(res) => {
-          console.log("Files: ", res);
-          setImageURL(res[0].url);
-        }}
-        onUploadError={(error: Error) => {
-          alert(`ERROR! ${error.message}`);
-        }}
-      ></UploadDropzone>
-      {imageURL.length ? (
-        <div>
-          <Image src={imageURL} alt="image" width={500} height={300}></Image>
-        </div>
-      ) : null}
-    </div>
+    <>
+      <div>
+        <UploadDropzone
+          endpoint="strictImageAttachment"
+          onClientUploadComplete={(res) => {
+            setImageURL(res[0].url);
+          }}
+          onUploadError={(error: Error) => {
+            alert(`ERROR! ${error.message}`);
+          }}
+        ></UploadDropzone>
+        {imageURL.length ? (
+          <div>
+            <Image src={imageURL} alt="image" width={500} height={300}></Image>
+          </div>
+        ) : null}
+      </div>
+      <Result url={imageURL} />
+    </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
-import Link from "next/link";
 import { ImageUpload } from "./components/image-upload";
 
 export default function Home() {
   return (
-    <div className="mt-16 min-w-[500px] ">
+    <div className="mt-16 min-w-[600px]">
       <h1 className="font-bold text-5xl text-center text-slate-950">
         Upload your garbage image here!
       </h1>
-      <div className="flex justify-evenly w-full mt-10">
+      <div className="justify-center w-full mt-10 items-center flex flex-col">
         <ImageUpload />
-      </div>
-      <div className="flex justify-center items-center">
-        <div className="mt-10 bg-slate-200 rounded-lg border w-1/2">
-          <p className="text-3xl text-center">This is food garbage</p>
-        </div>
       </div>
     </div>
   );
